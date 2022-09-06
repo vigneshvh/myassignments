868eb56e95a6414768ab5d06216e6491006b37cf
vigneshvh <112793895+vigneshvh@users.noreply.github.com>
2022-09-06 19:53:40 +0530

    Update Checkthenumber.java

--- Checkthenumber.java
+++ Checkthenumber.java
@@ -7,9 +7,11 @@ public class Checkthenumber {
 		int num = -2;
 		if (num < 0)
 			System.out.println(num + "the given number is negative");
+		else if (num == 0)
+			System.out.println(num + "the given number is neither positive nor negative");
 		else
 			System.out.println(num + "the given number is positive");
-
 	}
 
 }
+
